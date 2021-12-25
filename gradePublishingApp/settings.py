fbf78c6d1628a67fba6d979d57a1623e82e92a08
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2021-12-25 09:41:13 +0200

    changed the database connection

--- gradePublishingApp/settings.py
+++ gradePublishingApp/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'gradePublishingApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'grade_publisher',
-        'USER': 'faisal',
-        'PASSWORD': 'Callme@123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
